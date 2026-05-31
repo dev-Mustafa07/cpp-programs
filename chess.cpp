11208bed4037961bf3a7d62b7ac84e4a6bcaeb56
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-05-31 23:35:44 +0500

    Basic structure ready

--- chess.cpp
+++ chess.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    // FILE INITIALIZE
+int main()
+{
+    int num;
+    cout << "Please enter number to print chess square: ";
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num; j++)
+        {
+            cout << "*\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
