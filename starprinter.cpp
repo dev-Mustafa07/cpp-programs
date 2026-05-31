09f6a4992140af209e9fbe1b66d5b6867b8c3597
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-05-31 23:13:57 +0500

    Star program completed

--- starprinter.cpp
+++ starprinter.cpp
@@ -1,16 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int num;
-    cout << "Please enter number: ";
+int main()
+{
+    int num, userchoice;
+    cout << "Please! Tell in whuch pattern you want to print star triangle\n";
+    // FIRST CHOICE
+    cout << "1. for\n";
+    cout << "*   *   *   *\n";
+    cout << "*   *   *\n";
+    cout << "*   *\n";
+    cout << "*\n";
+    // SECOND CHOICE
+    cout << "2. for\n";
+    cout << "*\n";
+    cout << "*   *\n";
+    cout << "*   *   *\n";
+    cout << "*   *   *   *\n";
+    cout << "Enter number: ";
+    cin >> userchoice;
+    cout << "Please enter number to print the no of stars ow want to print: ";
     cin >> num;
-    for(int i = 0; i <= num; i++){
-        for (int j = 0; j < i; j++)
+    if (userchoice == 1)
+    {
+        for (int i = 0; i <= num; i++)
         {
-            cout << "*\t";
+            for (int j = num; j > i; j--)
+            {
+                cout << "*\t";
+            }
+            cout << endl;
         }
-        cout << endl;
+    }
+    else if (userchoice == 2)
+    {
+        for (int i = 0; i <= num; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                cout << "*\t";
+            }
+            cout << endl;
+        }
     }
     return 0;
 }
