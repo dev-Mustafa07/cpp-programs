8b3d660a3743642fc49e739940ed628f11bc8b20
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-01 01:54:34 +0500

    Intro to array and Rollno Program made with bugs

---
+++ arrayintro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // This is my first [program] in which I used arrays in C++;
+    int nums[5] = {5, 7, 5, 4, 1};
+    cout << nums[2]<< "\n";
+    cout << "===============================================\n";
+    cout << "\n\n\tThank you to use our program\n";
+    cout << "\tDeveloper - Muhammad Mustafa\n";
+    cout << "===============================================";
+    return 0;
+}
---
+++ arraysearch.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userno;
+    bool checker = false;
+    int num = 10;
+    int rollNo[num] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+    cout << "Please enter your Roll no: ";
+    cin >> userno;
+    //  num = 10;
+    for (int i = 0; i < num; i++)
+    {
+        if (userno == rollNo[i])
+        {
+            // checker == true;
+            // break;
+            cout << userno << " - You are already enrolled!\n";
+        }
+        else
+        {
+            // cout << userno << " - You aren't enrolled!\n";
+            // checker == false;
+        }
+        // cout << checker << endl;
+    }
+    // if (checker)
+    // {
+    //     cout << userno << " - You are already enrolled!\n";
+    // }
+    // else
+    // {
+    //     cout << userno << " - You aren't enrolled!\n";
+    // }
+    cout << "===============================================\n";
+    cout << "\n\n\tThank you to use our program\n";
+    cout << "\tDeveloper - Muhammad Mustafa\n";
+    cout << "===============================================";
+    return 0;
+}
