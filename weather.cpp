40278f3a1944c30e7466e19faed863ac70db7a71
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-01 19:05:00 +0500

    Program completed

--- weather.cpp
+++ weather.cpp
@@ -1,107 +1,61 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     float temperature[7] = {};
-    int highest = temperature[0];
-    int lowest = temperature[0];
+    string days[7] = {"Monday", "Tuesday", "Wednesday", "Thursaday", "Friday", "Saturday", "Sunday"};
     for (int i = 0; i < 7; i++)
     {
         if (i == 0)
         {
-            cout << "Enter the temperature in Celsius of Monday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[0] > highest)
-            {
-                highest = temperature[0];
-            }
-            
-            if (temperature[0] < lowest)
-            {
-                lowest = temperature[0];
-            }
         }
         else if (i == 1)
         {
-            cout << "Enter the temperature in Celsius of Tuesday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[1] > highest)
-            {
-                highest = temperature[1];
-            }
-            if (temperature[1] < lowest)
-            {
-                lowest = temperature[1];
-            }
         }
         else if (i == 2)
         {
-            cout << "Enter the temperature in Celsius of Wednesday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[2] > highest)
-            {
-                highest = temperature[2];
-            }
-            if (temperature[2] < lowest)
-            {
-                lowest = temperature[2];
-            }
         }
         else if (i == 3)
         {
-            cout << "Enter the temperature in Celsius of Thursday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[3] > highest)
-            {
-                highest = temperature[3];
-            }
-            
-            if (temperature[3] < lowest)
-            {
-                lowest = temperature[3];
-            }
         }
         else if (i == 4)
         {
-            cout << "Enter the temperature in Celsius of Friday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[4] > highest)
-            {
-                highest = temperature[4];
-            }
-            
-            if (temperature[4] < lowest)
-            {
-                lowest = temperature[4];
-            }
         }
         else if (i == 5)
         {
-            cout << "Enter the temperature in Celsius of Saturday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[5] > highest)
-            {
-                highest = temperature[5];
-            }
-            if (temperature[5] < lowest)
-            {
-                lowest = temperature[5];
-            }
-
         }
         else if (i == 6)
         {
-            cout << "Enter the temperature in Celsius of sunday: ";
+            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-            if (temperature[6] > highest)
-            {
-                highest = temperature[6];
-            }
-            if (temperature[6] < lowest)
-            {
-                lowest = temperature[6];
-            }
+        }
+    }
+    int highest = temperature[0];
+    int lowest = temperature[0];
+    for (int j = 0; j < 7; j++)
+    {
+        if (temperature[j] > highest)
+        {
+            highest = temperature[j];
+        }
+
+        if (temperature[j] < lowest)
+        {
+            lowest = temperature[j];
         }
     }
     cout << "\nDaily temperatures recorded successfully!\n";
