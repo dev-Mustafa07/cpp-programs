2c148efe6c1f9f2d23fa21c85c34cbae443c60dd
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-01 20:07:50 +0500

    Code Optimized

--- weather.cpp
+++ weather.cpp
@@ -8,41 +8,8 @@ int main()
     string days[7] = {"Monday", "Tuesday", "Wednesday", "Thursaday", "Friday", "Saturday", "Sunday"};
     for (int i = 0; i < 7; i++)
     {
-        if (i == 0)
-        {
             cout << "Enter the temperature in Celsius of " << days[i] << ": ";
             cin >> temperature[i];
-        }
-        else if (i == 1)
-        {
-            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
-            cin >> temperature[i];
-        }
-        else if (i == 2)
-        {
-            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
-            cin >> temperature[i];
-        }
-        else if (i == 3)
-        {
-            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
-            cin >> temperature[i];
-        }
-        else if (i == 4)
-        {
-            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
-            cin >> temperature[i];
-        }
-        else if (i == 5)
-        {
-            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
-            cin >> temperature[i];
-        }
-        else if (i == 6)
-        {
-            cout << "Enter the temperature in Celsius of " << days[i] << ": ";
-            cin >> temperature[i];
-        }
     }
     int highest = temperature[0];
     int lowest = temperature[0];
