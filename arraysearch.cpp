781a414c7034c1a55fa71087a5506fa46f146532
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-01 09:07:01 +0500

    Lts code

--- arraysearch.cpp
+++ arraysearch.cpp
@@ -4,35 +4,36 @@ using namespace std;
 int main()
 {
     int userno;
-    bool checker = false;
-    int num = 10;
-    int rollNo[num] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+    bool checker;
+    int rollNo[10] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
     cout << "Please enter your Roll no: ";
     cin >> userno;
     //  num = 10;
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i < 10; i++)
     {
         if (userno == rollNo[i])
         {
-            // checker == true;
-            // break;
-            cout << userno << " - You are already enrolled!\n";
+            checker = true;
+            break;
+            // cout << userno << " - You are already enrolled!\n";
         }
         else
         {
             // cout << userno << " - You aren't enrolled!\n";
-            // checker == false;
+            checker = false;
+            break;
+
         }
         // cout << checker << endl;
     }
-    // if (checker)
-    // {
-    //     cout << userno << " - You are already enrolled!\n";
-    // }
-    // else
-    // {
-    //     cout << userno << " - You aren't enrolled!\n";
-    // }
+    if (checker)
+    {
+        cout << userno << " - You are already enrolled!\n";
+    }
+    else
+    {
+        cout << userno << " - You aren't enrolled!\n";
+    }
     cout << "===============================================\n";
     cout << "\n\n\tThank you to use our program\n";
     cout << "\tDeveloper - Muhammad Mustafa\n";
