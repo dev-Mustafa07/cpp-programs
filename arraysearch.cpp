f08e337df3f6eac9e69b94052801d79531648a56
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-01 09:34:06 +0500

    Program Finalized

--- arraysearch.cpp
+++ arraysearch.cpp
@@ -4,36 +4,42 @@ using namespace std;
 int main()
 {
     int userno;
+    char userchoice;
     bool checker;
     int rollNo[10] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
-    cout << "Please enter your Roll no: ";
-    cin >> userno;
-    //  num = 10;
-    for (int i = 0; i < 10; i++)
+    cout << "===============================================\n";
+    cout << "\n\n\tWELCOME TO HARVARD UNIVESITY\n\n";
+    cout << "===============================================\n";
+    do
     {
-        if (userno == rollNo[i])
+        cout << "\nPlease enter your Roll no: ";
+        cin >> userno;
+        //  num = 10;
+        for (int i = 0; i < 10; i++)
         {
-            checker = true;
-            break;
-            // cout << userno << " - You are already enrolled!\n";
+            if (userno == rollNo[i])
+            {
+                checker = true;
+                break;
+            }
+            else
+            {
+                checker = false;
+            }
+        }
+        if (checker)
+        {
+            cout << userno << " - You are already enrolled!\n";
         }
         else
         {
-            // cout << userno << " - You aren't enrolled!\n";
-            checker = false;
+            cout << userno << " - You aren't enrolled!\n";
         }
-        // cout << checker << endl;
-    }
-    if (checker)
-    {
-        cout << userno << " - You are already enrolled!\n";
-    }
-    else
-    {
-        cout << userno << " - You aren't enrolled!\n";
-    }
-    cout << "===============================================\n";
-    cout << "\n\n\tThank you to use our program\n";
+        cout << "Do you want to continue a program(y/n): ";
+        cin >> userchoice;
+    } while (userchoice == 'y' || userchoice == 'Y');
+    cout << "\n===============================================\n";
+    cout << "\n\tThank you to use our program\n";
     cout << "\tDeveloper - Muhammad Mustafa\n";
     cout << "===============================================";
     return 0;
