bdedc146c8e4bf2fdb6f74d13b0bf8160446c588
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-03 01:52:25 +0500

    Function added

--- converter.cpp
+++ converter.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int decimalno, rem;
+void converter(int num){
     int binary[32] = {};
     int i = 0;
-    cout << "Enter your Decimal number to convert it into binary number: ";
-    cin >> decimalno;
-    while (decimalno > 0)
+    while (num > 0)
     {
-        binary[i] = decimalno % 2;
-        decimalno = decimalno / 2;
+        binary[i] = num % 2;
+        num = num / 2;
         i++;
     }
     for (int j = i-1; j >= 0; j--)
     {
         cout << binary[j];
     }
+}
+
+int main()
+{
+    int decimalno;
+    cout << "Enter your Decimal number to convert it into binary number: ";
+    cin >> decimalno;
+    converter(decimalno);
     return 0;
 }
