0bb8cfa88c08c9209252d84803d7d2c6822bd882
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-03 02:59:29 +0500

    Program Pending

--- bin2dec.cpp
+++ bin2dec.cpp
@@ -7,28 +7,18 @@ int main(){
     int num;
     int dec;
     int i = 0;
-    int array[num];
     cout << "How many bits do you have for your binary number: ";
     cin >> num;
+    int array[num];
     cout << "Enter you binary number to convert that in Decimal number containing " << num << " bits: ";
     cin >> dec;
-    // i = 0
-    // array[4]
-    // num = 4
-    // dec = 1011
     while(dec > 0){
-        dec = dec / 10;
+        dec = dec % 10;
         array[i] = dec;
-        // 1.loop 
-        //  dec = 101
-        //  array[0] = 101
         dec = dec * pow(array[i], i);
-        // dec = 0
+        dec = dec / 10;
         i++;
     }
-    // cout << "\n-----\n";
-    // cout << ans;
-    // cout << "\n-----\n";
 
     for(int j = 0;j < num;j++){
         ans = array[j] + ans;
