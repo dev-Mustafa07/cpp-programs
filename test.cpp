7740c35d6d7d7596f6041fd9a52490758d2fc7cf
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-06 23:13:21 +0500

    Big milestone

--- test.cpp
+++ test.cpp
@@ -3,28 +3,55 @@
 using namespace std;
 
 int main()
-{
-    int nums = 4;
-    int bin[nums] = {1, 10, 11, 100};
+{   
+    int nums;
+    cout << "Please enter how many binary numbers do you have? ";
+    cin >> nums;
+    int finans = 0;
+    int f = 0;
+    int bin[nums] = {}; // Store all binary numbers
+    int dec[nums] = {}; // Store all decimal numbers that user input
+    do
+    {
+        cout << "Enter you binary number: ";
+        cin >> bin[f];
+       f++;
+    } while (f < nums);
+    
     for (int j = 0; j < nums; j++)
     {
         int exp = 0;
-        
+
         int arrayno, singleno = 0, ans = 0;
         arrayno = bin[j];
         while (arrayno > 0)
         {
-            // cout << "\n start of while loop \n";
             singleno = arrayno % 10;
             singleno = singleno * pow(2, exp);
             arrayno /= 10;
             exp++;
-            
+
             ans = singleno + ans;
-            // cout << "\n End of while loop \n";
         }
-        cout << ans;
-        cout << "\t";
+        dec[j] = ans;
     }
+
+    for (int l = 0; l < nums; l++)
+    {
+        finans += dec[l];
+    }
+    int finbins[32] = {};
+    int p = 0;
+    while (finans > 0)
+    {
+        finbins[p] = finans % 2;
+        finans /= 2;
+        p++;
+    }
+    for (int g = p - 1; g >= 0; g--)
+    {
+        cout << finbins[g];
+    }
+
     return 0;
 }
