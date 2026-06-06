a57848e9e12ba00563db0f2c9b86841f9353a730
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-06 17:20:19 +0500

    Binary number sun program initialize

--- test.cpp
+++ test.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int i;
-    for(i = 1; i < 10; i++){
-        cout << i << endl;
+    int exp = 0;
+    int bin[4] = {1, 10, 11, 100};
+    for (int j = 0; j < 4; j++)
+    {
+        int dec[16] = {};
+        int arrayno, singleno, ans;
+        arrayno = bin[j];
+        while (arrayno > 0)
+        {
+            // cout << "\n start of while loop \n";
+            singleno = arrayno % 10;
+            singleno = singleno * pow(2, exp);
+            cout << " - " << singleno << " - ";
+            dec[j] = singleno;
+            arrayno /= 10;
+            exp++;
+            // cout << "\n End of while loop \n";
+        }
+        for (int k = 0; k < 16; k++)
+        {
+            ans = dec[k] + ans;
+            // cout << "\n End of for loop \n";
+        }
+        cout << ans;
+        cout << "\n";
     }
-    cout <<"\n" << i;
     return 0;
 }
