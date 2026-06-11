82e505a821f0040f70e9f58bb8db82afce875eb2
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-11 23:24:37 +0500

    Vector intro and memory printer

---
+++ Vector/Vectorintro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector> // *IMP to import the file of vector
+using namespace std;
+
+int main(){
+    vector <int> vec = {1,2,3};
+    for(int i : vec){
+    cout << vec[0];
+
+    }
+    return 0;
+}
