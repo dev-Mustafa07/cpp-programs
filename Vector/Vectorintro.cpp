9f20723b58552f1fbe555e52dca4757e3ba59a7d
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-12 15:43:05 +0500

    Vector Functions

--- Vector/Vectorintro.cpp
+++ Vector/Vectorintro.cpp
@@ -2,11 +2,22 @@
 #include <vector> // *IMP to import the file of vector
 using namespace std;
 
-int main(){
-    vector <int> vec = {1,2,3};
-    for(int i : vec){
-    cout << vec[0];
+int main()
+{ 
+    vector<int> vec; // Create an empty array
+    vector<int> vec2 = {1,2,3}; // Create an with elements
+    vector<int> vec3 (3 ,0); // Create an with same elements and specific size
 
-    }
+    
+    cout << "Very first size of vector(before pushing): " << vec.size() << endl;
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    cout << "After pushing element in vector: " << vec.size() << endl;
+    vec.pop_back();
+    cout << "Front and back" << endl;
+    cout << vec.front() << endl;
+    cout << vec.back() << endl;
+    cout << vec.at(1) << endl;
     return 0;
 }
