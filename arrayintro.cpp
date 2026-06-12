8853d6c3794b1e064156521e154db409aca66cb1
Muhammad Mustafa <dev.muhammadmustafa07@gmail.com>
2026-06-12 20:17:59 +0500

    Program with memory location

--- arrayintro.cpp
+++ arrayintro.cpp
@@ -1,10 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     // This is my first [program] in which I used arrays in C++;
     int nums[5] = {5, 7, 5, 4, 1};
-    cout << nums[2]<< "\n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << &nums[i] << "\n";
+    }
     cout << "===============================================\n";
     cout << "\n\n\tThank you to use our program\n";
     cout << "\tDeveloper - Muhammad Mustafa\n";
